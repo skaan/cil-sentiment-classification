edc2a5e04d16dea76168bd5a96ae38b4c1a59f9a
PhilMoesch <p.moesch@hotmail.ch>
2020-04-28 15:25:15 +0200

    parser

---
+++ preprocessing/parser.py
@@ -0,0 +1,25 @@
+#inputfile = open('sample.txt')
+#outputfile = open('sampleout.txt','w')
+from preprocessing_interface import PreprocessingInterface
+import re
+
+class parser(PreprocessingInterface):
+
+    def run(self):
+        output = open(self.output, 'a+')
+        with open(self.input, 'r', encoding='utf8') as inputfile:
+            next(inputfile)
+            next(inputfile)
+
+            i = 0
+            for line in inputfile:
+                if (i%4 == 0):
+                    tmp = " ".join(filter(lambda x:x[0]!='@' and x[0:4]!='http', line.split()))
+                    tmp = re.sub('([.,!?()])', r' \1 ', tmp)
+                    tmp = re.sub('\s{2,}', ' ', tmp)
+                    output.write(tmp + "\n")
+                i = i+1
+
+parser = parser()
+parser.set_paths('sample.txt','sampleout.txt')
+parser.run()
