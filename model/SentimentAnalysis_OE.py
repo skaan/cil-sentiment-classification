38258e6b430615504f12ec3233e5e3dd2f5fcbcf
Ferdi K <ferdi.kossmann@outlook.com>
2020-07-26 16:52:43 +0200

    val split

--- model/SentimentAnalysis_OE.py
+++ model/SentimentAnalysis_OE.py
@@ -1,6 +1,6 @@
 #%%
 
-# importring libraries 
+# importring libraries
 import os
 from argparse import Namespace
 from collections import Counter
@@ -9,7 +9,7 @@ import re
 import string
 import numpy as np
 import pandas as pd
-import torch 
+import torch
 from tqdm import tqdm
 import torch.nn as nn
 import torch.nn.functional as F
@@ -19,6 +19,7 @@ from torch.autograd import Variable
 from torch.utils.data import TensorDataset
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data.dataset import random_split
+from sklearn.model_selection import train_test_split,ShuffleSplit
 from tqdm import tqdm_notebook
 import nltk.data
 import nltk
@@ -32,30 +33,31 @@ import six
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('stopwords')
-#importing module 
-import logging 
-  
-#Create and configure logger 
-logging.basicConfig(filename="BILSTM.log", 
-                    format='%(asctime)s %(message)s', 
-                    filemode='w') 
+#importing module
+import logging
+from torch.utils.tensorboard import SummaryWriter
 
-#Creating an object 
-logger=logging.getLogger() 
-  
-#Setting the threshold of logger to DEBUG 
-logger.setLevel(logging.DEBUG) 
-#%%
+#Create and configure logger
+logging.basicConfig(filename="checkpoint/BILSTM.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
 
+#Creating an object
+logger=logging.getLogger()
+
+#Setting the threshold of logger to DEBUG
+logger.setLevel(logging.DEBUG)
 #%%
-## Data Loading 
+writer = SummaryWriter('runs/BILSTM_train')
+#%%
+## Data Loading
 
 def load_directory_data(file):
   """Retrieves the Sentences from the input text file into a Dict,stores and return into Dataframe """
   data = {}
   data["sentence"] = []
-  num_lines = sum(1 for line in open(file,'r'))
-  with open(file,'r') as f:
+  num_lines = sum(1 for line in open(file,'r',encoding='utf-8'))
+  with open(file,'r',encoding='utf-8') as f:
     print('\n opened file : '+file)
     for line in tqdm(f, total=num_lines):
         data["sentence"].append(line)
@@ -96,13 +98,13 @@ def loadGloveModel():
 def fetch_and_load_datasets():
   """ Initialises both train and test dataframes"""
   oldpwd=os.getcwd()
-  os.chdir("../data/no_mst/")
+  os.chdir("../data/cleaned/")
   train_df = load_dataset(os.getcwd(),True)
   test_df = load_dataset(os.getcwd(),False)
   os.chdir(oldpwd)
   GloVe = loadGloveModel()
   return train_df,test_df,GloVe
-  
+
 
 
 #%%
@@ -118,16 +120,16 @@ class SentAnaVocabulary(object):
     self._token_to_idx = token_to_idx
 
     self._idx_to_token = {idx: token for token, idx in self._token_to_idx.items()}
-    
+
     self._add_unk = add_unk
     self._unk_token = unk_token
     self._mask_token = mask_token
-    
+
     self.mask_index = self.add_token(self._mask_token)
     self.unk_index = -1
     if add_unk:
-        self.unk_index = self.add_token(unk_token) 
-    
+        self.unk_index = self.add_token(unk_token)
+
   def add_token(self, token):
     """Update mapping dicts based on the token."""
     if token in self._token_to_idx:
@@ -137,7 +139,7 @@ class SentAnaVocabulary(object):
         self._token_to_idx[token] = index
         self._idx_to_token[index] = token
     return index
-        
+
   # def get_embedding(self, token):
   #     """Add a list of tokens into the Vocabulary"""
   #     if token in self.Glove:
@@ -168,7 +170,7 @@ class SentAnaVocabulary(object):
 
 ## Sentence Vectorizer
 class SentAnaVectorizer(object):
-  """ The Vectorizer which coordinates the Vocabularies and puts them to use"""    
+  """ The Vectorizer which coordinates the Vocabularies and puts them to use"""
   def __init__(self, SA_vocab):
       """
       Assigns an Class Global Object for further vectorizing of data after getting initialised SentAnaVocabulary Object
@@ -212,7 +214,7 @@ class SentAnaDataset(Dataset):
     """
     self.sa_df = sa_df
     self._vectorizer = vectorizer
-    
+
     measure_len = lambda sentence: len(sentence.split(" "))
     self._max_seq_length = min(max(map(measure_len, sa_df.sentence)),max_seq_len)
 
@@ -229,7 +231,7 @@ class SentAnaDataset(Dataset):
 
   def __len__(self):
     return self._target_size
-    
+
   def get_num_batches(self, batch_size):
     """Given a batch size, return the number of batches in the dataset"""
     return len(self) // batch_size
@@ -253,22 +255,22 @@ class SentimentRNN(nn.Module):
         self.output_size = output_size
         self.n_layers = n_layers
         self.hidden_dim = hidden_dim
-        # embedding 
+        # embedding
         def create_embedding_layer(weights_matrix):
           "Takes Embedding as matrix and load into the embedding layer"
           num_embeddings, embedding_dim = weights_matrix.shape
           emb_layer = nn.Embedding.from_pretrained(torch.FloatTensor(weights_matrix),padding_idx=1)
           return emb_layer, num_embeddings, embedding_dim
-      
+
         emb_layer , vocab_size, embedding_dim = create_embedding_layer(weights_matrix)
         self.embedding = emb_layer
 
         # LSTM LAyer
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers,dropout=drop_prob, batch_first=True,bidirectional=True)
-        
+
         # dropout layer
         self.dropout = nn.Dropout(0.3)
-        
+
         # linear and sigmoid layer
         self.decoder1 = nn.Linear(hidden_dim*2, hidden_dim)
         self.decoder2 = nn.Linear(hidden_dim,output_size)
@@ -286,13 +288,13 @@ class SentimentRNN(nn.Module):
         embeds = self.embedding(x)
 
 
-        #packed_embedded = nn.utils.rnn.pack_padded_sequence(embeds, sentence_length, batch_first=True) 
-        lstm_out, hidden = self.lstm(embeds, hidden)   
+        #packed_embedded = nn.utils.rnn.pack_padded_sequence(embeds, sentence_length, batch_first=True)
+        lstm_out, hidden = self.lstm(embeds, hidden)
 
 
         # lstm_fw = lstm_out[:, :, :self.hidden_dim]
         # lstm_bw = lstm_out[:, :, self.hidden_dim:]
-        
+
         #Fetching the hidden state of Backward and Forward
         lstm_out = torch.cat((hidden[0][-2, :, :], hidden[0][-1, :, :]), dim=1)
 
@@ -304,22 +306,22 @@ class SentimentRNN(nn.Module):
 
         lstm_out = self.decoder2(lstm_out)
 
-        return F.log_softmax(lstm_out),hidden
- 
-    
+        return F.log_softmax(lstm_out,dim=1),hidden
+
+
     def init_hidden(self, batch_size):
         ''' Initializes hidden state '''
         # Create two new tensors with sizes n_layers x batch_size x hidden_dim,
         # initialized to zero, for hidden state and cell state of LSTM
         weight = next(self.parameters()).data
-        
+
         if(self.device):
           hidden = (weight.new(self.n_layers*2, batch_size, self.hidden_dim).zero_().cuda(),
                    weight.new(self.n_layers*2, batch_size, self.hidden_dim).zero_().cuda())
         else:
           hidden = (weight.new(self.n_layers*2, batch_size, self.hidden_dim).zero_(),
                    weight.new(self.n_layers*2, batch_size, self.hidden_dim).zero_())
-        
+
         return hidden
 
 
@@ -327,7 +329,7 @@ class SentimentRNN(nn.Module):
 
 print("Loading dataset")
 train_df , test_df ,GloVe= fetch_and_load_datasets()
-dataset = SentAnaDataset.load_dataset(train_df)    
+dataset = SentAnaDataset.load_dataset(train_df)
 vectorizer = dataset.get_vectorizer()
 
 print("Creating the embedding matrix")
@@ -336,34 +338,46 @@ weights_matrix = np.zeros((matrix_len, 200))
 words_found = 0
 
 for i, word in enumerate(vectorizer.SA_vocab._token_to_idx):
-    try: 
+    try:
         weights_matrix[i] = GloVe[word]
         words_found += 1
     except KeyError:
         weights_matrix[i] = np.random.normal(scale=0.6, size=(200,))
 
 
+train_df, val_df = train_test_split(train_df,shuffle=True,train_size=0.9,test_size=0.1,random_state=42)
 
+train_df.reset_index(drop=True,inplace=True)
+
+val_df.reset_index(drop=True,inplace=True)
 
 outputs = []
 for index,row in train_df.iterrows():
   outputs.append(torch.tensor(vectorizer.vectorize(row['sentence'],140)))
 
+val_outputs = []
+for index,row in val_df.iterrows():
+  val_outputs.append(torch.tensor(vectorizer.vectorize(row['sentence'],140)))
+
 test_outputs = []
 for index,row in test_df.iterrows():
   test_outputs.append(torch.tensor(vectorizer.vectorize(row['sentence'],140)))
 
+#%%
 print("Created the embedding matrix")
 train_x = torch.stack(outputs)
-train_y = torch.tensor(train_df['polarity'], dtype=torch.long)
+train_y = torch.tensor(train_df['polarity'].values, dtype=torch.long)
+
+val_x = torch.stack(val_outputs)
+val_y = torch.tensor(val_df['polarity'].values, dtype=torch.long)
 
 test_x = torch.stack(test_outputs)
 
 train_data=TensorDataset(train_x,train_y)
-
+val_data = TensorDataset(val_x,val_y)
 test_data=TensorDataset(test_x)
 
-TrainData, ValidationData = random_split(train_data,[int(0.9*len(train_data)),len(train_data) - int(0.9*len(train_data))])
+#TrainData, ValidationData = random_split(train_data,[int(0.9*len(train_data)),len(train_data) - int(0.9*len(train_data))])
 
 pad_idx = vectorizer.SA_vocab.lookup_token('<MASK>')
 HIDDEN_DIM = 256
@@ -388,16 +402,35 @@ Device = torch.device("cuda" if cuda_available else "cpu")
 
 
 model = SentimentRNN(weights_matrix, HIDDEN_DIM, OUTPUT_DIM, BATCH_SIZE, N_LAYERS)
-train_loader = DataLoader(dataset=TrainData, batch_size=BATCH_SIZE,shuffle=True, drop_last=True)
-valid_loader = DataLoader(dataset=ValidationData, batch_size=BATCH_SIZE,shuffle=True, drop_last=True)
+train_loader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE,shuffle=True, drop_last=True)
+valid_loader = DataLoader(dataset=val_data, batch_size=BATCH_SIZE,drop_last=True)
 test_loader = DataLoader(dataset=test_data, batch_size=100)
 loss_func = nn.NLLLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learningrate)
 loss_func.to(Device)
+my_lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=0.9)
 model.train()
 
 
-# Model training 
+#%%
+def classDefiner(x):
+    if x[0] > x[1]:
+        return -1
+    return 1
+
+def accuracy(predicted,labels):
+    acc = 0
+    preds = map(classDefiner,list(predicted.cpu().detach().numpy()))
+    labels = list(labels.cpu().detach().numpy())
+    preds = list(preds)
+    for i in range(0,len(preds)):
+      if preds[i] == labels[i]:
+        acc+=1
+    return acc/len(preds)
+
+
+#%%
+# Model training
 
 epochs = 10
 clip=5 # gradient clipping
@@ -414,11 +447,13 @@ epoch_bar.n=0
 # train for some number of epochs
 for e in range(epochs):
     epoch_bar.update()
+    epoch_bar.set_postfix(lr=learningrate)
     # batch loop
     model.train()
     train_bar.n = 0
     val_bar.n = 0
     running_loss = 0
+    train_accuracy = 0
     hidden = model.init_hidden(BATCH_SIZE)
     index = 0
     for inputs, labels in train_loader:
@@ -441,16 +476,18 @@ for e in range(epochs):
         loss = loss_func(predictions, labels)
         loss.backward(retain_graph=True)
         running_loss += (loss.detach()  - running_loss) / (index + 1)
+        train_accuracy += accuracy(predictions,labels) / (index + 1)
         index+=1
-        nn.utils.clip_grad_norm_(model.parameters(), clip)  
+        nn.utils.clip_grad_norm_(model.parameters(), clip)
         optimizer.step()
-        train_bar.set_postfix(loss=running_loss, epoch=e)
+        train_bar.set_postfix(loss=running_loss,acc=train_accuracy, epoch=e)
         # loss stats
 
     valid_hidden = model.init_hidden(BATCH_SIZE)
     val_losses = []
     model.eval()
     running_val_loss = 0
+    val_accuracy = 0
     index = 0
     for inputs, labels in valid_loader:
         val_bar.update()
@@ -460,32 +497,69 @@ for e in range(epochs):
         valid_hidden = tuple([each.data for each in valid_hidden])
 
         output,valid_hidden = model(inputs,valid_hidden)
-                
+
         val_loss = loss_func(output, labels)
         running_val_loss += (val_loss.detach()  - running_val_loss) / (index + 1)
+        val_accuracy += accuracy(predictions,labels) / (index + 1)
         index+=1
         val_losses.append(val_loss.item())
-        val_bar.set_postfix(loss=val_loss, epoch=e)
-    
-    logger.info('train_loss'+str(running_loss)+'at epoch'+str(e+1))
-    logger.info('train_loss'+str(running_val_loss)+'at epoch'+str(e+1))
+        val_bar.set_postfix(loss=val_loss,acc=val_accuracy,epoch=e)
+
+    # Optimizer Learning Rate
+    learningrate = optimizer.param_groups[0]['lr']
+
+    #LearningRate Scheduler
+    my_lr_scheduler.step()
+
+    writer.add_scalar('Loss/train', running_loss, e)
+    writer.add_scalar('Loss/val', running_val_loss, e)
+    writer.add_scalar('Accuracy/train', train_accuracy, e)
+    writer.add_scalar('Accuracy/val', val_accuracy, e)
+
+    logger.info('train_loss == '+str(running_loss)+'at epoch'+str(e+1))
+    logger.info('val_loss == '+str(running_val_loss)+'at epoch'+str(e+1))
+    logger.info('train_acc == '+str(train_accuracy)+'at epoch'+str(e+1))
+    logger.info('val_acc == '+str(val_accuracy)+'at epoch'+str(e+1))
     torch.save({
     'epoch': e+1,
     'model_state_dict': model.state_dict(),
     'optimizer_state_dict': optimizer.state_dict(),
     'train_loss': running_loss,
-    'val_loss': running_val_loss
-    }, 'entire_model_onlyBiLSTM'+str(e+1)+'.pt')
+    'val_loss': running_val_loss,
+    'train_acc': train_accuracy,
+    'val_acc': val_accuracy,
+    }, 'checkpoint/entire_model_BiLSTM_'+str(e+1)+'.pt')
 
+
+valid_hidden = model.init_hidden(BATCH_SIZE)
+model.eval()
+val_op = []
+for inputs, labels in valid_loader:
+    if(cuda_available):
+        inputs, labels = inputs.cuda(), labels.cuda()
+
+    valid_hidden = tuple([each.data for each in valid_hidden])
+
+    val_predictions,valid_hidden = model(inputs,valid_hidden)
+
+    op3 = val_predictions.cpu()
+    val_preds = map(classDefiner,list(op3.detach().numpy()))
+    for item in list(val_preds):
+      val_op.append(item)
+
+print(val_df.shape)
+print(len(val_op))
+
+val_df['predictions'] = np.zeros(val_df.shape[0])
+
+for i in range(0,len(val_op)):
+  val_df.loc[i,'predictions'] = val_op[i]
+
+val_df.to_csv('checkpoint/Misclassification.csv')
 
 model.eval()
 test_hidden = model.init_hidden(100)
 print(type(test_loader))
-
-def classDefiner(x): 
-    if x[0] > x[1]:
-        return -1
-    return 1
 
 id=1
 
@@ -496,18 +570,16 @@ for inputs in test_loader:
     test_hidden = tuple([each.data for each in test_hidden])
 
     test_predictions,test_hidden = model(inputs,test_hidden)
-    with open('output_onlyBiLSTM.txt','a+',encoding ="utf-8") as fp:
+    with open('checkpoint/output_BiLSTM_new.txt','a+',encoding ="utf-8") as fp:
         op2 = test_predictions.cpu()
         preds = map(classDefiner,list(op2.detach().numpy()))
         for item in list(preds):
             fp.write("{},{}\n".format(id,item))
             id+=1
 
-PATH = "entire_model_onlyBiLSTM.pt"
+PATH = "checkpoint/entire_model_BiLSTM_New.pt"
 
 # Save
 torch.save(model, PATH)
-  
+
 # %%
-
-
